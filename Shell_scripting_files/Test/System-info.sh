764349484ccb4abebfa8bdca9d01dd81ee54cbee
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-16 20:28:31 +0530

    System info shell file added

---
+++ Shell_scripting_files/Test/System-info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##############################
+# Author : Parthasarathy
+# Date : 15/01/2025
+#
+# This Script outputs the node health
+#
+# Version : v1
+######################### 
+
+df - h
+
+free -g
+
+nproc
