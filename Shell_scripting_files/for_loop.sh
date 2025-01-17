ad5baa66ab67c2cf739e3e6e403e396fcd053f8d
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-17 09:06:18 +0530

    For loop shell file added and edited

---
+++ Shell_scripting_files/for_loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+################################
+#Name : Parthasarathy G
+#Date : 17/01/2025
+################################
+
+#iterations -> for
+
+
+for i in {1.100}; do echo $1; done
+
