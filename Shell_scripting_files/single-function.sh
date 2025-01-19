9321ea1fa6e1c83d2ca0834bf914593b02882899
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-19 22:15:50 +0530

    while-loop added

---
+++ Shell_scripting_files/single-function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+####################################
+#Author : Parthasarathy G
+#Date : 16/01/2025
+#Description : Basic shell scripting
+#####################################
+
+# define a function to print a greeting
+sayhello() {
+  echo "Hello, $1!"
+}
+# call the sayhello function with a name parameter
+sayhello "Rahul"
+
